2cbd626b3f990a4c667a34cc28a021e48983106d
Maksim Ezhov <maxim.ezhov88@mail.ru>
2023-02-28 12:43:12 +0300

    Домашняя работа

---
+++ Task02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+int a, b;
+Console.WriteLine("Введите два числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Первое число " + a + " больше чем второе " + b);
+}
+else
+{
+    Console.WriteLine("Второе число " + b + " больше чем первое " + a);
+}
+Console.ReadLine();
---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+int i = 1;
+int N = 5;
+Console.WriteLine("Введите число:");
+N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + N);
+while (i <= N)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
+    Console.WriteLine();
